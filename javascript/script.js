25d142898fd8345362e64d95ee26fda8e385cdbd
Vimmi05 <vimmituli1999@gmail.com>
2023-07-12 10:10:23 +0530

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+gsap.from('#nav',{duration:2, y:'-100%',ease:'bounce'})
+
+gsap.from('#img1',{duration:2,y:'-50%',rotation: '360' ,ease:'elastic'})
+
+gsap.to('#img2',{duartion:2,y:'-25%',rotation:'-360',ease:'elastic'})
+
+gsap.from("#img3",{duration:2,rotation:'-360'})
